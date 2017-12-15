5d77c3b4acebf5780276194501cbc43ffb79e72c
Lenny Peters <mr.lennypeters@gmail.com>
2017-12-15 11:41:16 -0800

    master - refactor -
    added combineReducers from redux to combine both reducers used in the reducers file so the redux state can have both in the tree

--- src/components/App.js
+++ src/components/App.js
@@ -1,46 +1,40 @@
 import React, { Component } from 'react';
-import { addRecipe } from '../actions';
+import { connect } from 'react-redux';
+import { addRecipe, removeFromCalendar } from '../actions';
 
 class App extends Component {
-
-    state = {
-        calender: null
-    }
-
-    componentDidMount () {
-        const { store } = this.props
-    
-        store.subscribe(() => {
-          this.setState(() => ({
-            calendar: store.getState()
-          }));
-        });
-      }
-
-    submitFood = () => {
-        this.props.store.dispatch(addRecipe({
-            day: 'monday',
-            meal: 'breakfast',
-            recipe: {
-                label: this.input.value
-            },
-        }));
-
-        this.input.value = ''
-    }
-
     render () {
+        console.log('Props', this.props);
         return (
             <div>
-                <input type="text" ref={(input) => this.input = input} placeholder="Monday's Breakfast"/>
-                <button onClick={this.submitFood}>Submit</button>
-                
-                <pre>
-                    Monday's Breakfast: {this.state.calender && this.state.calender.monday.breakfast}
-                </pre>
+                Hello World
             </div>
         )
     }
 }
 
-export default App;
+function mapStateToProps ({ calendar, food }) {
+    const dayOrder = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+
+    return {
+        calendar: dayOrder.map((day) => ({
+        day,
+        meals: Object.keys(calendar[day]).reduce((meals, meal) => {
+            meals[meal] = calendar[day][meal]
+            ? food[calendar[day][meal]]
+            : null
+
+            return meals;
+        }, {})
+        })),
+    }
+}
+
+function mapDispatchToProps (dispatch) {
+  return {
+    selectRecipe: (data) => dispatch(addRecipe(data)),
+    remove: (data) => dispatch(removeFromCalendar(data))
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
